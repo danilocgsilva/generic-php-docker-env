8e9ccb18504ab5068b7e763de1d65ec3dadd4705
Danilo Silva <contato@danilocgsilva.me>
2023-01-31 08:28:40 -0300

    Removed unnecessary files from old pattern

--- clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf file_envs/node_modules
-rm -rf file_envs/vendor
-rm -rf file_envs/.env
-rm file_envs/package-lock.json
-rm file_envs/composer.lock
--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -p "Is your application already in the file_envs folder?" RESPONSE
-
-if [[ $RESPONSE = "yes" ]]; then
-    docker-compose up --build
-else
-    echo The files from application still are in the right place.
-fi
